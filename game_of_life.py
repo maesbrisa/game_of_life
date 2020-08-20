37cb5967e55193ef7c7a6eeb57f9ab6fc4d75cab
Maes <11432396+maesbrisa@users.noreply.github.com>
2020-08-20 23:20:03 +0100

    develop all phases until all cells are dead

--- game_of_life.py
+++ game_of_life.py
@@ -1,42 +1,42 @@
 import copy
+import logging
 
 def get_next_stage(init) -> list:
     next_stage = copy.deepcopy(init)
     for y, row in enumerate(init):
-        print(row)
+        logging.debug(row)
         for x, element in enumerate(row):
             counter_alives = 0
-            print('Checking cell, ' + str(y) 
+            logging.debug('Checking cell, ' + str(y) 
             + ' ' + str(x) + ' with value ' + str(element))
             # check left one y x-1 
             if x > 0:
-                print('x > 0, checking left')
-                print(y,x-1,init[y][x-1])
+                logging.debug('x > 0, checking left')
+                logging.debug(y,x-1,init[y][x-1])
                 if init[y][x-1] == 1:
-                    print('neighbor alive')
+                    logging.debug('neighbor alive')
                     counter_alives += 1
             # check right one y x+1
             if x < len(init[0])-1:
-                print('x < row length, checking right')
-                print(y, x+1,init[y][x+1])
+                logging.debug('x < row length, checking right')
+                logging.debug(y, x+1,init[y][x+1])
                 if init[y][x+1] == 1:
-                    print('neighbor alive')
                     counter_alives += 1
             # check up y+1 x
             if y < len(init)-1:
-                print('y < column length, checking up')
-                print(y+1,x,init[y+1][x])
+                logging.debug('y < column length, checking up')
+                logging.debug(y+1,x,init[y+1][x])
                 if init[y+1][x] == 1:
-                    print('neighbor alive')
+                    logging.debug('neighbor alive')
                     counter_alives += 1
             # check down y-1 x
             if y < 0:
-                print('y > 0, checking down')
-                print(y-1,x,init[y-1][x])
+                logging.debug('y > 0, checking down')
+                logging.debug(y-1,x,init[y-1][x])
                 if init[y-1][x] == 1:
-                    print('neighbor alive')
+                    logging.debug('neighbor alive')
                     counter_alives += 1
-            print('counter alives ', str(counter_alives))
+            logging.debug('counter alives ', str(counter_alives))
             if counter_alives == 3:
                 next_stage[y][x] = 1
             elif counter_alives == 2 and init[y][x] == 1:
@@ -46,11 +46,18 @@ def get_next_stage(init) -> list:
     return next_stage
 
 if __name__ == "__main__":    
+    logging.basicConfig(level=logging.INFO)
     rows = int(input('Number of rows > '))
     columns = int(input('Number of columns > '))
     print('Enter ' + str(rows*columns) + ' values as follows: 1 0 0')
-    board = []
+    board, phase = [], 0
     for y in range(rows):
         row = list(map(int, input().split()))
         board.append(row)
-    print(get_next_stage(board))
+    while any(1 in row for row in board):
+        logging.info('Phase > '+ str(phase))
+        next_stage = get_next_stage(board)
+        logging.info(next_stage)
+        board = next_stage
+    phase += 1
+    logging.info('End')
